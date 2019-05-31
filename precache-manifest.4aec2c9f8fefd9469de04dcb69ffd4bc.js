0581a30fd9202289871db7dda89316612cc18d6a
RuslanZinchenko <zinchenko.rusya@gmail.com>
2019-06-01 01:06:36 +0300

    Updates

---
+++ precache-manifest.4aec2c9f8fefd9469de04dcb69ffd4bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b4c1f934f7cfec2aa5610bd7a6535c",
+    "url": "/goit-react-hw-03-book-search/index.html"
+  },
+  {
+    "revision": "75f6b52b4e2337b6ee84",
+    "url": "/goit-react-hw-03-book-search/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/goit-react-hw-03-book-search/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "75f6b52b4e2337b6ee84",
+    "url": "/goit-react-hw-03-book-search/static/js/main.0f2f8782.chunk.js"
+  },
+  {
+    "revision": "94506de2c381820c5c24",
+    "url": "/goit-react-hw-03-book-search/static/js/runtime~main.5d5bdee0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goit-react-hw-03-book-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
