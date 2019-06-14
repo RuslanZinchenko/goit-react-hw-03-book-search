7385e0630ac73911f748d2f77f1be5e4755b8ea2
RuslanZinchenko <38359557+RuslanZinchenko@users.noreply.github.com>
2019-06-14 12:10:46 +0300

    Update article-api.js

--- src/services/article-api.js
+++ src/services/article-api.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
 
 /* eslint-disable-next-line */
-export const fetchArticles = (query = 'react', category = 'computers') =>
+export const fetchArticles = (query = 'react', category = '') =>
   axios.get(`${BASE_URL}+${query}+subject:${category}`);
